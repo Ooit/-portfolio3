c53a75af750bc9bcc557dc9fb3fc28c92b29bf59
Rob <rob.nard@gmail.com>
2017-03-14 16:30:48 -0400

    Hobbies Structure
    Added the basic markup for the hobbies section

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,11 @@
-var landingHeight = document.getElementById("landing_section").offsetHeight,
-landingCTA = document.getElementById("landing_cta_button"),
-landingHeadline = document.getElementById("landing_headline_text").offsetHeight,
-landingSubtext = document.getElementById("landing_subtext_text").offsetHeight,
-ctaMargin = landingHeight - landingHeadline - landingSubtext;
+//Band-aid for positioning the CTA button in the Landing section to the bottom.
+(function() {
 
-//alert(ctaMargin);
+    var landingHeight = document.getElementById("landing_section").offsetHeight,
+    landingCTA = document.getElementById("landing_cta_button"),
+    landingHeadline = document.getElementById("landing_headline_text").offsetHeight,
+    landingSubtext = document.getElementById("landing_subtext_text").offsetHeight,
+    ctaMargin = landingHeight - landingHeadline - landingSubtext;
 
-landingCTA.style.marginTop = ctaMargin / 1.3;
+    landingCTA.style.marginTop = ctaMargin / 1.3;
+})();
